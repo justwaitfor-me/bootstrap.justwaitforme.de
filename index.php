710a04b5a1e10154bbcd4591894ac1ec5dcfc2d3
Just Wait <reimdiego@icloud.com>
2025-02-03 21:18:32 +0100

    Refactor .htaccess files and update favicon assets; remove obsolete files and add new styles

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap Editor</title>
-    <link rel="shortcut icon" href="public/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="src/img/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sidebars/">
-    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="src/css/style.css">
 </head>
 
 <body>
@@ -82,8 +82,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
         </script>
-    <script src="public/js/main.js"></script>
-    <script type="module" src="public/js/listener.js"></script>
+    <script src="src/js/main.js"></script>
+    <script type="module" src="src/js/listener.js"></script>
 </body>
 
 </html>
